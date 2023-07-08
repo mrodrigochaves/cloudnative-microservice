7af4ffe4f4eeea5fe410de83834fb8260fc7d276
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-07 21:10:12 -0300

    update native test

--- requestblockchain/src/native-test/java/com/mrodrigochaves/RequestResourceIT.java
+++ requestblockchain/src/native-test/java/com/mrodrigochaves/RequestResourceIT.java
@@ -3,6 +3,6 @@ package com.mrodrigochaves;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+public class RequestResourceIT extends RequestServiceTest {
     // Execute the same tests but in packaged mode.
 }
